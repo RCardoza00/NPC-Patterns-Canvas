db16286c9151c7ebf3c159fddb001cc15057aaec
Ruben Cardoza <rubencardoza611@gmail.com>
2022-08-01 04:20:10 -0700

    sine wave patterns

--- script.js
+++ script.js
@@ -3,29 +3,34 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
+const numberOfEnemies = 70;
 const enemiesArray = [];
-const enemyImage = new Image();
-enemyImage.src = './img/enemy1.png'
 let gameFrame = 0;
 
 
 class Enemy{
   constructor(){
-    this.x = Math.random() *CANVAS_WIDTH;
-    this.y = Math.random() *CANVAS_HEIGHT;
-    this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image = new Image();
+    this.image.src = './img/enemy3.png';
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth/2.5;
     this. height = this.spriteHeight/2.5;
+    this.x = Math.random() *(CANVAS_WIDTH - this.width);
+    this.y = Math.random() *(CANVAS_HEIGHT - this.height);
     this.frame = 0;
     this.flapSpeed =Math.floor (Math.random() * 3 + 1);
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 1.5 + 0.5;
+    this.curve = Math.random() * 200 + 50;
   }
   update(){
-    this.x+=this.speed;
-    this.y+=this.speed;
+    this.x= canvas.width/2 * Math.cos(this.angle * Math.PI/90) + (canvas.width/2-this.width/2);
+    this.y= canvas.height/2 *  Math.sin(this.angle * Math.PI/270) + (canvas.height/2-this.height/2);
+    this.angle+=this.angleSpeed; 
     //animate sprites
+    if (this.x + this.width < 0) this.x = canvas.width;
     if (gameFrame % this.flapSpeed ===0){
         this.frame > 4 ? this.frame=0 : this.frame++;
     }
@@ -34,7 +39,7 @@ class Enemy{
   }
   draw(){
     //ctx.strokeRect(this.x,this.y,this.width,this.height);
-    ctx.drawImage(enemyImage,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,
+    ctx.drawImage(this.image,this.frame*this.spriteWidth,0,this.spriteWidth,this.spriteHeight,
                   this.x, this.y,this.width,this.height);
   }
 }
